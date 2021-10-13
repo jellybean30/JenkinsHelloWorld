65151459227cb272804f86380ecd098d8c3a4b69
jellybean30 <88818036+jellybean30@users.noreply.github.com>
2021-10-13 20:39:35 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("-----***** Hello World & welcome to Jenkins *****-----");
+		System.out.println("-----***** Made some changes *****-----");
 
 	}
 
